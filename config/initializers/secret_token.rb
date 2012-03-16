e32fccf8643dd2839f677e7ebaa84a31257103fd
Yazz Atlas <entropyworks@gmail.com>
2012-03-16 02:52:00 -0700

    adding all the files for the first_app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'f0590b8fd6c0fe0bbe81d988d89b209c25507dda45ee86b1d92c096aa347856b1e95d41697317ad3821214aa65faaf6df869a4e150309e200bc8fb3a7e4b1aae'
